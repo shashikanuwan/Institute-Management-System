549134458df5497c933a6a2566ea74009f3c7838
shashika nuwan <kumararanaweera1999@gmail.com>
2022-01-21 16:38:43 +0530

    setup navigation and create home controller

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,8 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                @if (Route::has('login'))
+                @auth
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -38,14 +40,27 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
                 </x-dropdown>
+
+                @else
+
+                <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                    {{ __('login') }}
+                </x-nav-link>
+                @if (Route::has('register'))
+                <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                    {{ __('register') }}
+                </x-nav-link>
+
+                @endif
+                @endauth
+                @endif
             </div>
 
             <!-- Hamburger -->
@@ -62,6 +77,8 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        @if (Route::has('login'))
+        @auth
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
@@ -80,13 +97,25 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
         </div>
+
+        @else
+        <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
+            {{ __('Login') }}
+        </x-responsive-nav-link>
+        @if (Route::has('register'))
+
+        <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
+            {{ __('register') }}
+        </x-responsive-nav-link>
+        @endif
+        @endauth
+        @endif
     </div>
 </nav>
