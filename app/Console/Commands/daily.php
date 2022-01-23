ea970a4928e8b238f1329b44f74fa8969bc498ef
shashika nuwan <kumararanaweera1999@gmail.com>
2022-01-23 06:55:38 +0530

    Create a cron command to delete the previously completed classes

---
+++ app/Console/Commands/daily.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class daily extends Command
+{
+    protected $signature = 'daily:delete';
+
+    protected $description = 'This will delete the previously completed classes';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function handle()
+    {
+        DB::table('programs')
+        ->where('start_date_time', "<", Carbon::now() )
+        ->delete();
+    }
+}
